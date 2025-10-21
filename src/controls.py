64647727dc4880fc6748080d8c5ca425bb93100f
Joseph Elliott <jwe23@fsu.edu>
2025-10-21 01:36:07 -0400

    Update controls.py

--- src/controls.py
+++ src/controls.py
@@ -5,8 +5,7 @@ import numpy as np
 class Controls:
     def __init__(self):
         self.pitch_rate = 0.03
-        self.throttle_rate = 0.015
-        self.max_pitch = np.pi / 3
+        self.throttle_rate = 0.01
     
     def process_input(self, aircraft):
         keys = pygame.key.get_pressed()
@@ -17,6 +16,11 @@ class Controls:
             aircraft.throttle = max(0.0, aircraft.throttle - self.throttle_rate)
         
         if keys[pygame.K_UP]:
-            aircraft.pitch = min(self.max_pitch, aircraft.pitch + self.pitch_rate)
+            aircraft.pitch += self.pitch_rate
         if keys[pygame.K_DOWN]:
-            aircraft.pitch = max(-self.max_pitch, aircraft.pitch - self.pitch_rate)
+            aircraft.pitch -= self.pitch_rate
+        
+        if aircraft.pitch > 2 * np.pi:
+            aircraft.pitch -= 2 * np.pi
+        if aircraft.pitch < -2 * np.pi:
+            aircraft.pitch += 2 * np.pi
