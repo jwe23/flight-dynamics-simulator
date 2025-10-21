7c0aae8166a07c29299ade2c2cda9dacdb206afa
Joseph Elliott <jwe23@fsu.edu>
2025-10-21 01:10:08 -0400

    Update aircraft initial parameters

--- src/aircraft.py
+++ src/aircraft.py
@@ -3,11 +3,11 @@ import numpy as np
 
 class Aircraft:
     def __init__(self):
-        self.position = np.array([0.0, 0.0, 150.0])
-        self.velocity = np.array([60.0, 0.0, 0.0])
-        self.pitch = 0.05  # Slight positive pitch
+        self.position = np.array([0.0, 0.0, 200.0])
+        self.velocity = np.array([50.0, 0.0, 0.0])
+        self.pitch = 0.0
         
-        self.mass = 1000.0
+        self.mass = 1200.0
         
-        self.throttle = 0.6
+        self.throttle = 0.5
         self.pitch_input = 0.0
