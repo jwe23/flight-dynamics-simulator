048a4323d6a17c5205cef2128383a43aae302d9c
Joseph Elliott <jwe23@fsu.edu>
2025-10-21 01:03:55 -0400

    Adjust initial position and velocity of Aircraft

--- src/aircraft.py
+++ src/aircraft.py
@@ -3,11 +3,11 @@ import numpy as np
 
 class Aircraft:
     def __init__(self):
-        self.position = np.array([0.0, 0.0, 100.0])
-        self.velocity = np.array([50.0, 0.0, 0.0])
+        self.position = np.array([0.0, 0.0, 200.0])  # Start higher
+        self.velocity = np.array([80.0, 0.0, 0.0])   # Start faster for lift
         self.pitch = 0.0
         
         self.mass = 1000.0
         
-        self.throttle = 0.5
+        self.throttle = 0.7  # Start with more throttle
         self.pitch_input = 0.0
